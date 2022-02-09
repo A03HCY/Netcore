0b28db28cd32ad6acc0df49af71d0a03bec4941a
A03HCY <acdphc@qq.com>
2022-02-09 21:51:59 +0800

    remake

---
+++ Protocol.md
@@ -0,0 +1,13 @@
+# Basic Data Protocol
+
+## Head
+
+**1B** Type identification code
+
+**1B** Content identification code
+
+**4B** Content length code
+
+## Content
+
+**~B** Raw data
--- Ucode.py
+++ Ucode.py
@@ -1,91 +1,48 @@
-import socket, struct, base64
-import threading, queue
-import platform, os, io
-import random
-import ast
-import time
-import re
+from base64 import b64encode, b64decode
+from io import BytesIO
+from random import sample
+from struct import pack
+from re import compile
 
-__version__ = '1.0.3-beta'
-
-SAFE_TIME = 5
-OUT_TIME  = 15
+__version__ = '202202.2120'
 
 
 def IsIP(str):
-    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    p = compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
     if p.match(str):return True
     else:return False
 
 
 def RanCode(num):
-    ran_str = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789', num))
+    ran_str = ''.join(sample('abcdefghijklmnopqrstuvwxyz0123456789', num))
     return ran_str
 
 
 def B64en(data):
     data = data.encode('utf-8')
-    data = base64.b64encode(data).decode('utf-8')
+    data = b64encode(data).decode('utf-8')
     return data
 
 
 def B64de(data):
     data = data.encode('utf-8')
-    data = base64.b64decode(data).decode('utf-8')
+    data = b64decode(data).decode('utf-8')
     return data
 
 
-class Cosi:
-    def __init__(self, conn:socket.socket=None, mode:str='TCP', buffsize:int=2048) -> None:
-        self.Idata = {}
-        self.conn = conn
-        self.mode = mode
-        self.buff = buffsize
-        if conn == None:
-            if mode == 'TCP':
-                self.conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            elif mode == 'UDP':
-                self.conn = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+class Protocol:
+    def __init__(self, meta_data, type_code:int=0, content_code:int=0):
+        self.meta = meta_data
+        self.type_code = type_code
+        self.cont_code = content_code
     
-    def save(self, key, val):
-        self.Idata[key] = val
+    def GetHead(self, length:int=None):
+        iden_code = bytes( [self.type_code, self.cont_code] )
+        if not length:length = len(self.meta)
+        leng_code = pack('i', length)
+        head_code = iden_code + leng_code
+        return head_code
     
-    def get(self, key):
-        return self.Idata.get(key)
-    
-    def __send(self, bytesdata=b'', address=None) -> None:
-        if self.mode == 'UDP':
-            self.conn.sendto(address, bytesdata)
-            return
-        elif self.mode == 'TCP':
-            length = len(bytesdata)
-            header = struct.pack('i', length)
-            self.conn.send(header)
-            self.conn.send(bytesdata)
-            return
-        
-    def send(self, bytesdata=b'', address=None) -> None:
-        self.__send(bytesdata, address)
-    
-    def recv(self) -> bytes:
-        if self.mode == 'UDP':
-            bytesdatarray = self.conn.recvfrom()
-            return bytesdatarray
-        elif self.mode == 'TCP':
-            header = self.conn.recv(4)
-            length = struct.unpack('i', header)[0]
-            bytesdata = b''
-            while length > self.buff:
-                bytesdata += self.conn.recv(self.buff)
-                length -= self.buff
-            bytesdata += self.conn.recv(length)
-            return bytesdata
-    
-    def connect(self, address) -> None:
-        self.conn.connect(address)
-    
-    def bind(self, address) -> None:
-        self.conn.bind(address)
-    
-    def close(self) -> None:
-        self.conn.close()
+    def ReadHead(self, bytesdata:bytes):
+        self.type_code = bytesdata[0]
+        self.cont_code = bytesdata[1]
